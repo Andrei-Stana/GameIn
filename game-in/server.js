51bf8e8d066aabb2a1d5410c58210c7f8f6e1bef
Andrei-Lucian Stana <a.stana@HTBLALeonding.onmicrosoft.com>
2022-02-03 12:56:37 +0100

    Added login page

--- game-in/server.js
+++ game-in/server.js
@@ -3,8 +3,21 @@ exports.__esModule = true;
 var express = require('express');
 var cors = require('cors');
 var app = express();
+var auth = require('express-openid-connect').auth;
 var port = 3000;
 app.get('/', function (req, res) {
     res.json("Hello World");
 });
-app.listen(3000, function () { return console.log("Listening on port " + port); });
+app.listen(3000, function () { return console.log("Listening on port ".concat(port)); });
+var config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: '6I2ePLioTkFLHjWBFpNv0h372l6ZywnX',
+    issuerBaseURL: 'https://dev-y4mz30ly.us.auth0.com',
+    secret: '374eb7bb9b0e180fe0f578258f989f27724e537d27a569f335352e239300d3c0'
+};
+app.use(auth(config));
+app.get('/', function (req, res) {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
--- game-in/server.ts
+++ game-in/server.ts
@@ -5,6 +5,8 @@ const cors = require('cors');
 
 const app = express();
 
+const { auth } = require('express-openid-connect');
+
 const port : number = 3000
 
 app.get('/', (req, res) => {
@@ -12,3 +14,20 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, ()=> console.log(`Listening on port ${port}`))
+
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    baseURL: 'http://localhost:3000',
+    clientID: '6I2ePLioTkFLHjWBFpNv0h372l6ZywnX',
+    issuerBaseURL: 'https://dev-y4mz30ly.us.auth0.com',
+    secret: '374eb7bb9b0e180fe0f578258f989f27724e537d27a569f335352e239300d3c0'
+  };
+  
+  app.use(auth(config));
+  
+  
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+  });
