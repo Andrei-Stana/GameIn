ff7da17d729e2617af4c553a3a5ef9b3151ba855
Kyaw Htet Htun <kyawhtethtun65@.com>
2022-06-20 00:49:25 +0200

    Navbar change depend on auth state, Chat design changed

---
+++ Game-In/src/routes/profile/create.svelte
@@ -0,0 +1,18 @@
+<script>
+    let username;
+</script>
+<h1 class="display-2 text-center">Only 1 Step left</h1>
+<h1 class="display-4 text-center">"{username || "Your Name"}"</h1>
+<form class="container">
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">Username</span>
+        <input bind:value={username} type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    </div>
+    <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupFile01">Profile Picture</label>
+        <input type="file" class="form-control" id="inputGroupFile01" accept="image/*">
+    </div>
+    <div class="text-center">
+        <button class="btn btn-primary btn-lg">Create</button>
+    </div>
+</form>
